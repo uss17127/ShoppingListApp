b58a6e799bbb8dc134233847aace79c1c11f3963
Lauren-Lin <ll54463@uga.edu>
2022-11-19 15:23:19 -0500

    added image and buttons to home screen

--- app/src/main/java/edu/uga/cs/shoppinglistapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/MainActivity.java
@@ -1,14 +1,86 @@
 package edu.uga.cs.shoppinglistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
+import java.util.Arrays;
+import java.util.List;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String DEBUG_TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_YES );
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d( DEBUG_TAG, "JobLead: MainActivity.onCreate()" );
+
+        Button signInButton = findViewById( R.id.button1 );
+        Button registerButton = findViewById( R.id.button2 );
+
+
+    }
+
+
+
+
+
+
+    // These activity callback methods are not needed and are for educational purposes only
+    @Override
+    protected void onStart() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onStart()" );
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onResume()" );
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onPause()" );
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onStop()" );
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onDestroy()" );
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d( DEBUG_TAG, "ShoppingListApp: MainActivity.onRestart()" );
+        super.onRestart();
     }
 }
