9efbfe23e91a7ab744cc36c7784921e78b83280a
Lauren-Lin <ll54463@uga.edu>
2022-12-03 16:19:17 -0500

    created some xml

--- app/src/main/java/edu/uga/cs/shoppinglistapp/ReviewPurchasedItemsActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/ReviewPurchasedItemsActivity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,10 +26,9 @@ import java.util.List;
  * The current job leads are listed as a RecyclerView.
  */
 public class ReviewPurchasedItemsActivity
-        extends AppCompatActivity
-        implements EditItemDialogFragment.EditItemDialogListener {
+        extends AppCompatActivity {
 
-    public static final String DEBUG_TAG = "ReviewCartActivity";
+    public static final String DEBUG_TAG = "ReviewPurchasedActivity";
 
     private RecyclerView recyclerView;
     private ItemRecyclerAdapter recyclerAdapter;
@@ -39,18 +39,17 @@ public class ReviewPurchasedItemsActivity
 
     //Item list for checked items
     private List<Item> checkedItem = new ArrayList<>();
-    private  List<Integer> positionsList = new ArrayList<>();
+    private List<Integer> positionsList = new ArrayList<>();
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
+    protected void onCreate(Bundle savedInstanceState) {
 
-        Log.d( DEBUG_TAG, "onCreate()" );
+        Log.d(DEBUG_TAG, "onCreate()");
 
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_review_purchased_items);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_purchased_items);
 
-        recyclerView = findViewById( R.id.recyclerView );
-
+        recyclerView = findViewById(R.id.recyclerView);
 
         // initialize the Job Lead list
         itemsList = new ArrayList<Item>();
@@ -60,12 +59,16 @@ public class ReviewPurchasedItemsActivity
         recyclerView.setLayoutManager(layoutManager);
 
         // the recycler adapter with job leads is empty at first; it will be updated later
-        recyclerAdapter = new ItemRecyclerAdapter( itemsList, ReviewPurchasedItemsActivity.this);
-        recyclerView.setAdapter( recyclerAdapter );
+        recyclerAdapter = new ItemRecyclerAdapter(itemsList, ReviewPurchasedItemsActivity.this);
+        recyclerView.setAdapter(recyclerAdapter);
+
 
         // get a Firebase DB instance reference
         database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("cartlist");
+        DatabaseReference myRef = database.getReference("purchasedlist");
+
+
+
 
         // Set up a listener (event handler) to receive a value for the database reference.
         // This type of listener is called by Firebase once by immediately executing its onDataChange method
@@ -73,118 +76,32 @@ public class ReviewPurchasedItemsActivity
         //
         // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
         // to maintain job leads.
-        myRef.addValueEventListener( new ValueEventListener() {
+        myRef.addValueEventListener(new ValueEventListener() {
 
             @Override
-            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
                 itemsList.clear(); // clear the current content; this is inefficient!
-                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
                     Item item = postSnapshot.getValue(Item.class);
-                    item.setKey( postSnapshot.getKey() );
-                    itemsList.add( item );
-                    Log.d( DEBUG_TAG, "ValueEventListener: added: " + item );
-                    Log.d( DEBUG_TAG, "ValueEventListener: key: " + postSnapshot.getKey() );
+                    item.setKey(postSnapshot.getKey());
+                    itemsList.add(item);
+                    Log.d(DEBUG_TAG, "ValueEventListener: added: " + item);
+                    Log.d(DEBUG_TAG, "ValueEventListener: key: " + postSnapshot.getKey());
                 }
 
-                Log.d( DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter" );
+                Log.d(DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter");
                 recyclerAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled( @NonNull DatabaseError databaseError ) {
-                System.out.println( "ValueEventListener: reading failed: " + databaseError.getMessage() );
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("ValueEventListener: reading failed: " + databaseError.getMessage());
             }
-        } );
-    }
-
-
-    // This is our own callback for a DialogFragment which edits an existing item.
-    // The edit may be an update or a deletion of this item.
-    // It is called from the EditItemDialogFragment.
-    public void updateItem( int position, Item item, int action ) {
-        if( action == EditItemDialogFragment.SAVE ) {
-            Log.d( DEBUG_TAG, "Updating item at: " + position + "(" + item.getName() + ")" );
-
-            // Update the recycler view to show the changes in the updated job lead in that view
-            recyclerAdapter.notifyItemChanged( position );
-
-            // Update this item in Firebase
-            // Note that we are using a specific key (one child in the list)
-            DatabaseReference ref = database
-                    .getReference()
-                    .child( "cartlist" )
-                    .child( item.getKey() );
-
-            // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
-            // to maintain job leads.
-            ref.addListenerForSingleValueEvent( new ValueEventListener() {
-                @Override
-                public void onDataChange( @NonNull DataSnapshot dataSnapshot ) {
-                    dataSnapshot.getRef().setValue( item ).addOnSuccessListener( new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d( DEBUG_TAG, "updated item at: " + position + "(" + item.getName() + ")" );
-                            Toast.makeText(getApplicationContext(), "Item updated for " + item.getName(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-                @Override
-                public void onCancelled( @NonNull DatabaseError databaseError ) {
-                    Log.d( DEBUG_TAG, "failed to update item at: " + position + "(" + item.getName() + ")" );
-                    Toast.makeText(getApplicationContext(), "Failed to update " + item.getName(),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        else if( action == EditItemDialogFragment.DELETE ) {
-            Log.d( DEBUG_TAG, "Deleting item at: " + position + "(" + item.getName() + ")" );
-
-            // remove the deleted job lead from the list (internal list in the App)
-            itemsList.remove( position );
-
-            // Update the recycler view to remove the deleted job lead from that view
-            recyclerAdapter.notifyItemRemoved( position );
-
-            // Delete this job lead in Firebase.
-            // Note that we are using a specific key (one child in the list)
-            DatabaseReference ref = database
-                    .getReference()
-                    .child( "cartlist" )
-                    .child( item.getKey() );
-
-            // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
-            // to maintain job leads.
-            ref.addListenerForSingleValueEvent( new ValueEventListener() {
-                @Override
-                public void onDataChange( @NonNull DataSnapshot dataSnapshot ) {
-                    dataSnapshot.getRef().removeValue().addOnSuccessListener( new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d( DEBUG_TAG, "deleted item at: " + position + "(" + item.getName() + ")" );
-                            Toast.makeText(getApplicationContext(), "Item deleted for " + item.getName(),
-                                    Toast.LENGTH_SHORT).show();                        }
-                    });
-                }
-
-                @Override
-                public void onCancelled( @NonNull DatabaseError databaseError ) {
-                    Log.d( DEBUG_TAG, "failed to delete item at: " + position + "(" + item.getName() + ")" );
-                    Toast.makeText(getApplicationContext(), "Failed to delete " + item.getName(),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-    public void onItemCheck(Item item, int position) {
-        checkedItem.add(item);
-        positionsList.add(position);
-    }
-    public void onItemUncheck(Item item, int position) {
-        checkedItem.remove(item);
-        positionsList.remove(position);
+        });
     }
 }
+
+
+
+
