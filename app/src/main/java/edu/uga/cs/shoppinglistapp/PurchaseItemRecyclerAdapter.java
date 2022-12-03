86d493967d59bb4d9cc5a5fa566d799dffa9fb3f
Lauren-Lin <ll54463@uga.edu>
2022-12-03 16:42:40 -0500

    purchaseItemReycclcerAdapter added

---
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/PurchaseItemRecyclerAdapter.java
@@ -0,0 +1,91 @@
+package edu.uga.cs.shoppinglistapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * This is an adapter class for the RecyclerView to show all job leads.
+ */
+public class PurchaseItemRecyclerAdapter extends RecyclerView.Adapter<PurchaseItemRecyclerAdapter.ItemHolder> {
+
+    public static final String DEBUG_TAG = "PurchaseItemRA";
+
+    private List<Item> itemList;
+    private Context context;
+
+
+    public PurchaseItemRecyclerAdapter(List<Item> List, Context context) {
+        this.itemList = List;
+        this.context = context;
+    }
+
+    // The adapter must have a ViewHolder class to "hold" one item to show.
+    class ItemHolder extends RecyclerView.ViewHolder {
+
+        TextView itemName ;
+
+        public ItemHolder(View itemView ) {
+            super(itemView);
+            itemName = itemView.findViewById( R.id.itemName );
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder( ViewGroup parent, int viewType ) {
+        View view = LayoutInflater.from( parent.getContext()).inflate( R.layout.purchase_item, parent, false );
+        return new ItemHolder( view );
+    }
+
+    // This method fills in the values of the Views to show a JobLead
+    @Override
+    public void onBindViewHolder( ItemHolder holder, int position ) {
+        Item item = itemList.get( position );
+
+
+        Log.d( DEBUG_TAG, "onBindViewHolder: " + item );
+
+        String key = item.getKey();
+        String itemName = item.getName();
+        int itemAmount = item.getAmount();
+        double itemPrice = item.getPrice();
+
+
+        holder.itemName.setText( item.getName());
+
+
+
+        // We can attach an OnClickListener to the itemView of the holder;
+        // itemView is a public field in the Holder class.
+        // It will be called when the user taps/clicks on the whole item, i.e., one of
+        // the job leads shown.
+        // This will indicate that the user wishes to edit (modify or delete) this item.
+        // We create and show an EditJobLeadDialogFragment.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d( TAG, "onBindViewHolder: getItemId: " + holder.getItemId() );
+                //Log.d( TAG, "onBindViewHolder: getAdapterPosition: " + holder.getAdapterPosition() );
+                EditItemDialogFragment editItemFragment =
+                        EditItemDialogFragment.newInstance( holder.getAdapterPosition(), key, itemName, itemAmount, itemPrice);
+                editItemFragment.show( ((AppCompatActivity)context).getSupportFragmentManager(), null);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+}
